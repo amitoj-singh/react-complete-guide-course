ce4b0dfb2887697e67f8be430fac1909afce1b18
Amitoj Singh <amitoj.98@gmail.com>
2021-11-24 22:27:58 +0530

    assignment 2

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = props => {
+
+    const submitExpenseDataHandler = enteredExpenseData => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(),
+        }
+
+        props.onAddExpense(expenseData);
+    }
+
+    return (
+        <div className="new-expense">
+            <ExpenseForm onSubmitExpenseData={submitExpenseDataHandler} />
+        </div>
+    )
+}
+
+export default NewExpense;
